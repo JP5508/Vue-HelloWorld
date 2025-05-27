f5f041a754d85e91e298d84cf192fe7b76c7461f
JP5508 <contato.jp5508@gmail.com>
2025-05-27 17:44:21 -0300

    Adicionei mais parágrafos no Vue Adicionei explicação nas notas

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 //Importamos createApp diretamente da CDN do Vue 3.
 import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
 
-//Criamos um objeto App com um template contendo nosso "Hello, Vue.js!".
+//App é um objeto JavaScript que representa o componente raiz da sua aplicação.
+//Esse objeto possui uma propriedade chamada template, que é uma string HTML que o Vue vai renderizar.
 const App = {
-  template: `<h1>Hello, Vue.js!</h1>`
+  template: `
+  <h1>Hello World </h1>
+  <p>26/05/2025 - Eae mundo, meu Vue.js ta funcionando!</p>
+  <p>27/05/2025 - Entendi como funciona essa parte, vou ver mais coisas</p>
+  `
 }
-//createApp(App).mount('#app') monta o app dentro do elemento com id="app" no HTML.
+
+
+//createApp(App) cria a instância da aplicação Vue usando o componente App como raiz.
+//.mount('#app') diz para o Vue "renderizar e colocar essa aplicação dentro do elemento HTML que tem o id app".
 createApp(App).mount('#app')
